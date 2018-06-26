75bf2dbe795f020171de89a7da7b8b2c4326df4e
WillzMu <willzmu@yahoo.com>
2018-06-26 19:29:10 +0200

    improved the layout of categories section

--- src/Categories.js
+++ src/Categories.js
@@ -8,27 +8,14 @@ class Categories extends React.Component {
 
     render(){
       return <div>
-        <div className="container-fluid">
-      <Navbar title="Categories"/>
-     
     
-       
-        </div>
-        <div  className="center-align" style={{textAlign:"center"}}>
-        <div className="col-lg-6 ">
-        <div className="input-group">
-          <input type="text" className="form-control" placeholder="Search for..."/>
-          <span className="input-group-btn">
-            <button className="btn btn-default" type="button" >Go!</button>
-          </span>
-        </div>
-        </div>
-        <Tables />
+      <Navbar title="Categories"/>
       
-      </div>
+        <Tables/>
       </div>
     }
   }
+  
 
   var JobsSnapshot;
   var newArray = []
@@ -37,15 +24,18 @@ class Categories extends React.Component {
       super(props);
       this.state = {
         listOfPeople: [],
-        job : "Painter"
+        job : ""
       }
       this.handleClick = this.handleClick.bind(this);
+      
     };
-
+   
+  
    
     handleClick = (value) => {
       let peopleArray = [];
-
+        
+       
       //var jobs = firebase.database().ref('Jobs/'+ value);
       Firebase.database().ref('Jobs/'+ value).on('value',  (snapshot) => {
         JobsSnapshot = snapshot.val();
@@ -72,51 +62,81 @@ class Categories extends React.Component {
           listOfPeople:peopleArray
         })
        });
-       console.log(value)
+      //   console.log(value)
     }
     
 
 	render(){
     const {listOfPeople} = this.state;
-		return <div>
-      <div className="row">
-      <div>
-      <ButtonGroup vertical>
-      <Button  color="primary"  onClick={ ()=> this.handleClick("House Cleaner") }>House Cleaner</Button>
-      <Button color="primary" onClick={() => this.handleClick("Yard Cleaner")}>Yard Cleaner</Button>
-      <Button color="primary" onClick={() => this.handleClick("Carpenter")}>Carpenter</Button>
-     
-      </ButtonGroup>
-      
+		return <div className="row container-fluid justify-content-start mt-4">
+    <div className="card col-md-2 ml-3">
+      <div className="mt-3 justify-content-center text-center">
+        <h5>BROWSE JOBS</h5>
+        <hr/>
+      <div className="btn-group-vertical container-fluid">
+      {/* <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown
+    </button> */}
+      <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+        <a className="dropdown-item">House Help</a>
       </div>
-     <div className="row">
+      <button  type="button" class="btn btn-secondary" onClick={ ()=> this.handleClick("House Cleaner") }>House Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Yard Cleaner")}>Yard Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Carpenter")}>Carpenter</button>
+      {/* <button  type="button" class="btn btn-secondary" onClick={ ()=> this.handleClick("House Cleaner") }>Painter</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Yard Cleaner")}>Mechanic</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Carpenter")}>Hair-Dresser</button>
+      <button  type="button" class="btn btn-secondary" onClick={ ()=> this.handleClick("House Cleaner") }>House Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Yard Cleaner")}>Yard Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Carpenter")}>Carpenter</button>
+      <button  type="button" class="btn btn-secondary" onClick={ ()=> this.handleClick("House Cleaner") }>House Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Yard Cleaner")}>Yard Cleaner</button>
+      <button  type="button" class="btn btn-secondary" onClick={() => this.handleClick("Carpenter")}>Carpenter</button>
+      */}
+      </div>
+      </div>
+    </div>
+    <div  className="card col center-align mr-3 ml-3" style={{textAlign:"center"}}>
+        <div className="input-group mt-3 justify-content-center">
+          <input type="text" className="form-control col-6"
+           placeholder="I am looking to hire a..."/>
+          <span className="input-group-btn">
+            <button className="btn btn-default"
+          type="button" >Go!</button>
+          </span>
+        </div>
+        <div className="row">
          
+         
+         {
+           listOfPeople.forEach((element,i)=>{
+             newArray.push(Object.values(element))
+           })
+           
+           }
          
         {
-          listOfPeople.forEach((element,i)=>{
-            newArray.push(Object.values(element))
-          })
-          
-          }
-        
-{
-         newArray.map((element,i) => <div className="card" style={{width:200, margin:20}}  key={i}>
-
-             <img className="card-img-top" src= {element[2]["pic"]} alt={"profile pic"}/>
-         <h5 className="card-title">  {element[0]["firsName"]}</h5> 
-          <p className="card-text"> Profession: {element[1]["profession"]}   <br/>   
-            City:  {element[0]["city"]}     </p>
-           {/* {console.log(element)} */}
-          </div>
-          )
-          
-       }
-       { /*empty the array to avoid repetion of elements*/
-         newArray = []}
-
-      
-       </div>
+          newArray.map((element,i) => <div className="card" style={{width:200, margin:20}}  key={i}>
+ 
+              <img className="card-img-top" src= {element[2]["pic"]} alt={"profile pic"}/>
+          <h5 className="card-title">  {element[0]["firsName"]}</h5> 
+           <p className="card-text"> Profession: {element[1]["profession"]}   <br/>   
+             Rating : {element[1]["rating"]} <br/>  
+             Status : {element[1]["status"]} <br/>
+             City:  {element[0]["city"]}     </p> 
+             <button>Reviews</button>
+            {console.log(element)}
+           </div>
+           )
+           
+        }
+        { /*empty the array to avoid repetion of elements*/
+          newArray = []}
+ 
+       
+        </div> 
       </div>
+     
 		</div>
   }
 }
