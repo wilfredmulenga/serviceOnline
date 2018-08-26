4fd9d5a4526f2d3841a3224cd8978d3d5e15bae0
Olivier JM <manolivier93@gmail.com>
2018-08-26 16:32:29 +0200

    cleaned the Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Firebase from '../config/firebase';
 import Modal from 'react-modal';
-import Button from '@material-ui/core/Button';
-
-
-let loginStatus
 
 Modal.setAppElement('#root');
 class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSignOut = this.handleSignOut.bind(this);
-
-  }
-
-  handleSignOut() {
-    Firebase.auth().signOut();
-    this.setState({
-      signOutModalIsOpen: false,
-      signInModalIsOpen: true
-    })
-
-  }
-
   render() {
     return (
       <div className="App" id="navbar">
         {/* Navbar section */}
-        <nav className="navbar navbar-expand-lg "
-          style={{ backgroundColor: '#182157' }}>
-          <Link to="/" className="navbar-brand link" style={{lineHeight: '1.3',fontSize:'23px',
-                         letterSpacing: 'normal',fontWeight: 'bold',textTransform: 'capitalize'}}>
+        <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#182157' }}>
+          <Link
+            to="/"
+            className="navbar-brand link"
+            style={{
+              lineHeight: '1.3',
+              fontSize: '23px',
+              letterSpacing: 'normal',
+              fontWeight: 'bold',
+              textTransform: 'capitalize',
+            }}>
             Nchito
-
-         </Link>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -47,29 +32,21 @@ class Navbar extends React.Component {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse  navbar-right ">
-            {/* <img id='userPic' style={{ width: 40, height: 40, borderRadius: 20 }} /> */}
             <ul className="navbar-nav mr-auto ">
-
               <li className="nav-item active mr-3">
-                <Link to="/categories" className="link">Categories</Link>
+                <Link to="/categories" className="link">
+                  Categories
+                </Link>
               </li>
-              {/* <Link to="/login" className="link" //onClick={this.props.action} 
-                onClick={this.openSignInModal}>
-                <li className="nav-item active mr-3" id='login'>{
-                  (loginStatus) ? `Sign Out` : `Sign In`
-                }
-                </li>
-              </Link> */}
               <li className="nav-item active mr-3">
-                <Link to="/viewprofile" className="link">Profile</Link>
+                <Link to="/viewprofile" className="link">
+                  Profile
+                </Link>
               </li>
-              {/* <li className="nav-item active mr-3">
-                <Link to="/phonelogin" className="link">Login</Link>
-              </li> */}
             </ul>
           </div>
         </nav>
-      </div >
+      </div>
     );
   }
 }
